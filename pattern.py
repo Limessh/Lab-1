d8b6c8a7c04455c0fa5cc2a39cc2c969485c0e1a
Limessh <elizaveta.d.sh@gmail.com>
2024-10-12 16:26:32 +0300

    Up_1

--- pattern.py
+++ pattern.py
@@ -5,13 +5,15 @@ BLACK='\u001b[40m'
 diametr=6
 radius=diametr//2
 
-def draw_pattern(k):
-#узор будет выводиться k раз
-    print((f"{WHITE}{'   '*(radius-1)}{BLACK}{'   '*2}{WHITE}{'   '*(radius-1)}{WHITE}{'   '*(radius-1)}{BLACK}{'   '*2}{WHITE}{'   '*(radius-1)}{END}")*k)
-    print((f"{WHITE}{'   '*(radius-2)}{BLACK}{'   '*1}{WHITE}{'   '*2}{BLACK}{'   '*1}{WHITE}{'   '*(radius-2)}{WHITE}{'   '*(radius-2)}{BLACK}{'   '*1}{WHITE}{'   '*2}{BLACK}{'   '*1}{WHITE}{'   '*(radius-2)}{END}")*k)
-    print((f"{BLACK}{'   '*1}{WHITE}{'   '*4}{BLACK}{'   '*1}{BLACK}{'   '*1}{WHITE}{'   '*4}{BLACK}{'   '*1}{END}")*k)
-    print((f"{BLACK}{'   '*1}{WHITE}{'   '*4}{BLACK}{'   '*1}{BLACK}{'   '*1}{WHITE}{'   '*4}{BLACK}{'   '*1}{END}")*k)
-    print((f"{WHITE}{'   '*(radius-2)}{BLACK}{'   '*1}{WHITE}{'   '*2}{BLACK}{'   '*1}{WHITE}{'   '*(radius-2)}{WHITE}{'   '*(radius-2)}{BLACK}{'   '*1}{WHITE}{'   '*2}{BLACK}{'   '*1}{WHITE}{'   '*(radius-2)}{END}")*k)
-    print((f"{WHITE}{'   '*(radius-1)}{BLACK}{'   '*2}{WHITE}{'   '*(radius-1)}{WHITE}{'   '*(radius-1)}{BLACK}{'   '*2}{WHITE}{'   '*(radius-1)}{END}")*k)
-    
-print(draw_pattern(4))
+def draw_pattern(k,n):
+#узор будет выводиться k раз в длину
+#узор будет выводиться n раз в высоту
+    for i in range (n):
+        print((f"{WHITE}{'   '*(radius-1)}{BLACK}{'   '*2}{WHITE}{'   '*(radius-1)}{WHITE}{'   '*(radius-1)}{BLACK}{'   '*2}{WHITE}{'   '*(radius-1)}{END}")*k)
+        print((f"{WHITE}{'   '*(radius-2)}{BLACK}{'   '*1}{WHITE}{'   '*2}{BLACK}{'   '*1}{WHITE}{'   '*(radius-2)}{WHITE}{'   '*(radius-2)}{BLACK}{'   '*1}{WHITE}{'   '*2}{BLACK}{'   '*1}{WHITE}{'   '*(radius-2)}{END}")*k)
+        print((f"{BLACK}{'   '*1}{WHITE}{'   '*4}{BLACK}{'   '*1}{BLACK}{'   '*1}{WHITE}{'   '*4}{BLACK}{'   '*1}{END}")*k)
+        print((f"{BLACK}{'   '*1}{WHITE}{'   '*4}{BLACK}{'   '*1}{BLACK}{'   '*1}{WHITE}{'   '*4}{BLACK}{'   '*1}{END}")*k)
+        print((f"{WHITE}{'   '*(radius-2)}{BLACK}{'   '*1}{WHITE}{'   '*2}{BLACK}{'   '*1}{WHITE}{'   '*(radius-2)}{WHITE}{'   '*(radius-2)}{BLACK}{'   '*1}{WHITE}{'   '*2}{BLACK}{'   '*1}{WHITE}{'   '*(radius-2)}{END}")*k)
+        print((f"{WHITE}{'   '*(radius-1)}{BLACK}{'   '*2}{WHITE}{'   '*(radius-1)}{WHITE}{'   '*(radius-1)}{BLACK}{'   '*2}{WHITE}{'   '*(radius-1)}{END}")*k)
+        
+print(draw_pattern(4,4))
